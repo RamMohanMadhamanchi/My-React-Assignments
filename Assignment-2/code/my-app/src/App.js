cd40d8da39dd8bc51b8bbf54f44ffb2b4c658bf8
RamMohanMadhamanchi <rammohan.madhamanchi@gmail.com>
2022-03-29 15:30:22 +0530

    Assignment-2 Submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -8,7 +8,15 @@ function App(props) {
     
 return(
   <div className="container">
-  {/* Start your React code here */}
+  {/* Start your React code here */
+  <>
+  <div><img src ={employee.profileImg} className="profile-image" alt=""></img></div>
+  <div className="employee-name">{employee.name}</div>
+  <div className="employee-location">Location <p>{employee.location}</p></div>
+  <div className="employee-bloodGroup">Blood group <p>{employee.bloodGroup}</p></div>
+  <div className="employee-age">Age <p>{employee.age}</p></div>
+  </>
+  }
 </div>
 )
 }
